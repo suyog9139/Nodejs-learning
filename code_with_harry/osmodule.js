6f34e4b611a0120c3d7a1c9ef21ee584db6fb1b9
suyog9139 <suyogwalunj3@gmail.com>
2023-01-15 20:03:21 +0530

    Day2 02

---
+++ code_with_harry/osmodule.js
@@ -0,0 +1,13 @@
+const os = require('os');
+console.log(os.freemem());
+console.log(os.totalmem());
+console.log(os.homedir());
+console.log(os.hostname());
+console.log(os.platform());
+console.log(os.arch());
+console.log(os.type());
+console.log(os.uptime());
+console.log(os.release());
+console.log(os.userInfo());
+console.log(os.networkInterfaces());
+console.log(os.cpus());
