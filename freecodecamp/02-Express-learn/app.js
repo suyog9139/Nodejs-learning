2eda3d831e657040a4cd59a2b9e6b5e05550d01f
suyog9139 <suyogwalunj3@gmail.com>
2023-01-29 22:35:54 +0530

    Day7 6.43

--- freecodecamp/02-Express-learn/app.js
+++ freecodecamp/02-Express-learn/app.js
@@ -1,56 +1,35 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-const {products} = require('./data');
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+const logger = require('./logger')
+const authorize = require('./authorize')
 
+// req => middleware =>res
 
-app.get('/',(req,res)=>{
-    res.send('<h>Home Page</h><a href="/api/products">Products</a>')
+// use vs route
+// use is for middleware
+// route is for request handler
+// 2. options - our own / express / third party
+
+// app.use([logger, authorize])
+app.use(morgan('tiny'))
+app.get('/',(req,res) => {
+  
+  res.send('Home Page')
 })
 
-app.get('/api/products',(req,res)=>{
-    const newProducts = products.map((product)=>{
-        const {id,name,image} = product;
-        return {id,name,image}
-    })
-    res.json(newProducts)
+app.get('/about',(req,res) => {
+  res.send('About Page') 
 })
 
-app.get('/api/products/:productID',(req,res)=>{
-    const {productID} = req.params;
-    const singleProduct = products.find((product)=>product.id === Number(productID))
-    if(!singleProduct){
-        return res.status(404).send('Product Does Not Exist')
-    }
-    return res.json(singleProduct)
+app.get('/api/products',(req,res) => {
+    res.send('Products Page')
 })
 
-app.get('/api/v1/query',(req, res) => {
-    console.log(req.query)
-    const { search, limit } = req.query 
-    let sortedProducts = [...products]
-    if(search){
-        sortedProducts = sortedProducts.filter((product)=>{
-            return product.name.startsWith(search)
-        })
-    }
-    if(limit){
-        sortedProducts = sortedProducts.slice(0,Number(limit))
-
-    }
-    if(sortedProducts.length < 1){
-        return res.status(200).json({success:true,data:[]}) 
-    }
-    res.status(200).json(sortedProducts)
+app.get('/api/items',(req,res) => {
+    res.send('Items Page')
 })
 
-app.get('/api/products/:productID/reviews/:reviewID',(req,res)=>{
-    console.log(req.params)
-    res.send('Hello World')
+app.listen(5000, () => {
+  console.log('Server is listening on port 5000....')
 })
-
-
-app.listen(5000,()=>{
-    console.log('Server is listening on port 5000...')      
-})
-
